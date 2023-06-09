136b03d5fa51591cdcc94c0d3b55b0a89ad32ed9
Leon K <leon.kleinschmidt@outlook.de>
2023-06-09 20:27:38 +0200

    add test dataset and loader

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,9 +2,9 @@ import torch
 from torchvision import datasets
 
 
-def load_datasets(transform, batch_size):
+def load_datasets(transform, batch_size, train=True):
     dataset = datasets.MNIST(
-        root="../data", train=True, download=True, transform=transform
+        root="../data", train=True, download=train, transform=transform
     )
     loader = torch.utils.data.DataLoader(
         dataset=dataset, batch_size=batch_size, shuffle=True
