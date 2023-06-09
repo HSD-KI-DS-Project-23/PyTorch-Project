a23fc0554425663dca259371225a9efc5d111080
Leon K <leon.kleinschmidt@outlook.de>
2023-06-09 20:34:50 +0200

    add output settings, change data load behavior

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,9 +2,9 @@ import torch
 from torchvision import datasets
 
 
-def load_datasets(transform, batch_size, train=True):
+def load_datasets(transform, batch_size, train=True, download=True):
     dataset = datasets.MNIST(
-        root="../data", train=True, download=train, transform=transform
+        root="../data", train=train, download=download, transform=transform
     )
     loader = torch.utils.data.DataLoader(
         dataset=dataset, batch_size=batch_size, shuffle=True
