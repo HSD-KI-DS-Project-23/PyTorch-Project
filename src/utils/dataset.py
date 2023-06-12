f7e4be3154b401194377d311c390670a6eb05ea2
Leon K <leon.kleinschmidt@outlook.de>
2023-06-12 16:32:30 +0200

    add datasettype option

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,12 +2,20 @@ import torch
 from torchvision import datasets
 
 
-def load_datasets(transform, batch_size, train=True, download=True):
-    dataset = datasets.MNIST(
-        root="../data", train=train, download=download, transform=transform
-    )
-    loader = torch.utils.data.DataLoader(
-        dataset=dataset, batch_size=batch_size, shuffle=True
-    )
-
-    return dataset, loader
+def load_datasets(datasettype, transform, batch_size, train=True, download=True):
+    if datasettype == "MNIST":
+        dataset = datasets.MNIST(
+            root="../data", train=train, download=download, transform=transform
+        )
+        loader = torch.utils.data.DataLoader(
+            dataset=dataset, batch_size=batch_size, shuffle=True
+        )
+        return dataset, loader
+    elif datasettype == "CIFAR10":
+        dataset = datasets.CIFAR10(
+            root="../data", train=train, download=download, transform=transform
+        )
+        loader = torch.utils.data.DataLoader(
+            dataset=dataset, batch_size=batch_size, shuffle=True
+        )
+        return dataset, loader
