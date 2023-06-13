7bd5e61fdae939c1d4843ee6eccb26ad35c52437
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2023-06-13 15:29:28 +0200

    .

--- src/classes/gan/gan.py
+++ src/classes/gan/gan.py
@@ -21,8 +21,10 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, d_input_dim, d_hidden_dim):
+    def __init__(self, d_input_dim, d_hidden_dim, data_dim):
         super(Discriminator, self).__init__()
+
+        self.data_dim = data_dim
 
         self.model = nn.Sequential(
             nn.Linear(d_input_dim, d_hidden_dim),
@@ -36,4 +38,6 @@ class Discriminator(nn.Module):
         )
 
     def forward(self, x):
+        x.view(-1, self.data_dim)
+        print(x.ndim)
         return self.model(x)
