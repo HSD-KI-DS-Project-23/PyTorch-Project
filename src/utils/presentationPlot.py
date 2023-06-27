a10b934542c0adf040323ba84bea8cc2422ccf84
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2023-06-27 19:28:27 +0200

    add plotting of decoded

--- src/utils/presentationPlot.py
+++ src/utils/presentationPlot.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def presentation_plot(image, encoded, label):
+def presentation_plot(image, encoded, decoded, label):
     """Function which plots a MNIST picture, its encoded variant and the predicted label"""
 
-    fig, axs = plt.subplots(1, 3)
+    fig, axs = plt.subplots(1, 4)
 
     image = image.detach().numpy()
     encoded = encoded.detach().numpy()
+    decoded = decoded.detach().numpy()
     label = label.detach().numpy()
 
     # Plot the MNIST picture
@@ -16,17 +17,22 @@ def presentation_plot(image, encoded, label):
     axs[0].axis("off")
     axs[0].set_title("MNIST Picture")
 
-    # Plot the decoded tensor
+    # Plot the encoded tensor
     axs[1].imshow(encoded.squeeze(), cmap="gray")
     axs[1].axis("off")
     axs[1].set_title("Encoded Tensor")
 
+    # Plot the decoded tensor
+    axs[2].imshow(decoded.squeeze(), cmap="gray")
+    axs[2].axis("off")
+    axs[2].set_title("Decoded Picture")
+
     # Plot the predicted label
-    axs[2].bar(range(10), label.squeeze())
-    axs[2].set_xticks(np.arange(10))
-    axs[2].set_title("Predicted Label")
-    axs[2].set_xlabel("Digit")
-    axs[2].set_ylabel("Probability")
+    axs[3].bar(range(10), label.squeeze())
+    axs[3].set_xticks(np.arange(10))
+    axs[3].set_title("Predicted Label")
+    axs[3].set_xlabel("Digit")
+    axs[3].set_ylabel("Probability")
 
     plt.tight_layout()
     plt.show()
